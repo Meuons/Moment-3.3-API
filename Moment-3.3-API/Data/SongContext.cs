9baf0f51e86bc65429e9a03872e6be15572ef243
unknown <meouns>
2022-02-11 16:11:53 +0100

    first commit

---
+++ Moment-3.3-API/Data/SongContext.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Moment_3._3_API.Models;
+
+namespace Moment_3._3_API.Data
+{
+    public class SongContext : DbContext
+    {
+
+        public SongContext(DbContextOptions options) : base(options) 
+
+        {
+
+        }  
+
+        public DbSet<Song> songs { get; set; }
+    }
+}
---
+++ Moment-3.3-API/Models/Song.cs
@@ -0,0 +1,10 @@
+﻿namespace Moment_3._3_API.Models
+{
+    public class Song
+    {
+        public int SongId { get; set; }
+        public string Artist { get; set; } = string.Empty;
+        public int Length { get; set; }
+        public string Category { get; set; } = string.Empty;
+    }
+}
